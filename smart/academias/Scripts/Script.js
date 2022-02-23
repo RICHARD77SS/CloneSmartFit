bc258269d4f2b94cb86439555517a6341c0d5ddc
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-02-23 13:31:56 -0300

    Mudança no menu da pagina Academias, No menu Mais detalhes e nos links da pagina

--- smart/academias/Scripts/Script.js
+++ smart/academias/Scripts/Script.js
@@ -3,3 +3,27 @@
 function onClickMenu() {
     document.getElementById("menuDif").classList.toggle("change");
 }
+function onClickMenu2() {
+    document.getElementById("menu").classList.toggle("change")
+}
+function onClickMais() {
+    document.getElementById("ca").classList.toggle("change")
+}
+function onClickMaisb() {
+    document.getElementById("cb").classList.toggle("change")
+}
+function onClickMaisc() {
+    document.getElementById("cc").classList.toggle("change")
+}
+function onClickMaisd() {
+    document.getElementById("cd").classList.toggle("change")
+}
+function onClickMaise() {
+    document.getElementById("ce").classList.toggle("change")
+}
+function onClickMaisf() {
+    document.getElementById("cf").classList.toggle("change")
+}
+function onClickMaisg() {
+    document.getElementById("cg").classList.toggle("change")
+}
