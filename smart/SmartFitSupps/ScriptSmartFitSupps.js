4333d5b128dff90f75b1462625be21896ed3e5bc
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-03-07 15:41:37 -0300

    inicio do slider smartFitSupps e ajustes na pagina

--- smart/SmartFitSupps/ScriptSmartFitSupps.js
+++ smart/SmartFitSupps/ScriptSmartFitSupps.js
@@ -1,11 +1,26 @@
-const header = document.querySelector(".header")
+let time = 5000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img")
+max = images.length;
 
-function() {
-    window.scroll(function () {
-        if (this.scrollTop() > 160) {
-            header.addClass("fix");
-        } else {
-            header.removeClass("fix");
-        }
-    });
+function nextImage() {
+
+    images[currentImageIndex].classList.remove("selected")
+
+    currentImageIndex++
+
+    if (currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex].classList.add("selected")
 }
+
+function start() {
+    setInterval(() => {
+
+        nextImage()
+    }, time)
+}
+
+
+window.addEventListener("load", start)
